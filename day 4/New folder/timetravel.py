ca42231ba9592f51c77072c3d9137c0f02b0f28c
maariiami <mariami102@icloud.com>
2024-03-06 11:08:27 -0800

    commit

--- day 4/New folder/timetravel.py
+++
@@ -1,8 +0,0 @@
-name = input("enter your name: ")
-age = input("enter your age: ")
-goal = input("how many years into the future do you want to time travel? ")
-
-print(age + goal)
-
-
-
---
+++ day4/homework/timetravel.py
@@ -0,0 +1,8 @@
+name = input("enter your name: ")
+age = int(input("enter your age: "))
+future_time = input("how many years into the future do you want to time travel? ")
+
+print(int(age) +int (future_time))
+
+
+
