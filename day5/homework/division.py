59ef19e4811620241b3a9ba27241ab33746e73aa
maariiami <mariami102@icloud.com>
2024-03-07 09:14:58 -0800

    commit

---
+++ day5/homework/division.py
@@ -0,0 +1,59 @@
+num1 = 101
+num2 = 5
+print(num1 / num2)
+print(num1 // num2)
+print(num1 % num2)
+
+num1 = 50
+num2 = 2
+print(num1 / num2)
+print(num1 // num2)
+print(num1 % num2)
+
+num1 = 22
+num2 = 11
+print(num1 / num2)
+print(num1 // num2)
+print(num1 % num2)
+
+num1 = 205
+num2 = 5
+print(num1 / num2)
+print(num1 // num2)
+print(num1 % num2)
+
+num1 = 6
+num2 = 3
+print(num1 / num2)
+print(num1 // num2)
+print(num1 % num2)
+
+num1 = 10
+num2 = 10
+print(num1 / num1)
+print(num1 // num2)
+print(num1 % num2)
+
+num1 = 11
+num2 = 10
+print(num1 / num2)
+print(num1 // num2)
+print(num1 % num2)
+
+num1 = 21
+num2 = 10
+print(num1 / num2)
+print(num1 // num2)
+print(num1 % num2)
+
+num1 = 40
+num2 = 5
+print(num1 / num2)
+print(num1 // num2)
+print(num1 % num2)
+
+num1 = 18
+num2 = 3
+print(num1 / num2)
+print(num1 // num2)
+print(num1 % num2)
---
+++ day5/homework/type.py
@@ -0,0 +1,5 @@
+num1= int(input("please enter first number:  "))
+num2= int(input("please enfer second number:  "))
+ 
+print(type(num1))
+print(type(num2))
