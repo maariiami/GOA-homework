09d223e3d3f00e33b969ab7ad4174a1c865ea435
maariiami <mariami102@icloud.com>
2024-03-13 06:22:28 -0700

    commit

---
+++ day6/homework/day6.py
@@ -0,0 +1,29 @@
+
+#გავაკეთოთ სიმულაცია სადაც მომხმარებელს შეეძლება რეგისტრაცია, მომხმარებელი შემოიტანს 3 ცვლადს, სახელი, გვარი, ასაკი (int ფუნქცია არ არის სავალდებულო ასაკთან) საბოლოოდ ამ ყველაფერს print'ის მეშვეობით გამოიტანთ ტერმინალში.
+
+
+
+
+name = input("please enter your name: ")
+lastname = input("please enter your lastname: ")
+age = input("please enter your age: ")
+print("my name is" ,name, "my lastname" ,lastname, "I'm" ,age)
+
+
+
+#მომხმარებელს შემოატანინეთ 3 რიცხვი. შეინახეთ ისინი ცვლადებში და მათზე ცალცალკე გამოიტანეთ print'ის მეშვეობით გამრავლება, გაყოფა, მიმატება, გამოკლება. (თუ არ იცით კონკრეტული მათემატიკური ოპერაციები დასერჩეთ google'ში. e.g: how to multiply numbers in python)
+
+
+num1 = int(input("please enter first num: "))
+num2 = int(input("please enter second num: "))
+
+print(float(num1))
+
+print(str(num2))
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 // num2)
+print(num1 * num2)
+
+
