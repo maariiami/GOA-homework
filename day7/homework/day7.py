da3bc78c9535424ec44996c37b7baa7b12b946d8
maariiami <mariami102@icloud.com>
2024-03-14 01:08:08 -0700

    commit

---
+++ day7/homework/day7.py
@@ -0,0 +1,17 @@
+#რაც კი ვისწავლეთ შედარების ოპერატორები, მათემატიკური ოპერატორები, მექანიკური მონაცემთა ტიპის შეცვლა და ავტომატური ყველაფერზე ივარჯიშეთ და ნამუშევარი ატვირთე რაზეც ივარჯიშეთ
+
+
+num1 = 10
+num2 = 5
+
+print(num1 > num2)
+print(num1 < num2)
+print(num1 == num2)
+print(num1 >= num2)
+print(num1 <= num2)
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num1 // num2)
+print(num1 % num2)
