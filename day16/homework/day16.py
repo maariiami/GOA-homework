db4f2c7222a2a2b2d851bd0c1b9db359462dfd57
maariiami <mariami102@icloud.com>
2024-03-28 11:59:27 -0700

    commit

---
+++ day16/homework/day16.py
@@ -0,0 +1,26 @@
+#შექმენით სია სადაც შეინახავთ თქვენთვის საყვარელ ფილბეს და დაბეჭდეთ მთლიანი სია/ კონკრეტული ფილმი
+
+films = ["Life of Pi", "cast Away", "The Impossible", "The terminal", "The way back"]
+print(films)
+print(films[2])
+
+#შექმენით 2 სია და გააერთიანეთ ისინი
+
+girls = ["mari", "nini", "ani", "nata", "masho"]
+boys = ["tazo", "luks", "nika", "saba", "bacho"]
+print(girls + boys)
+
+#შექმენით სტრინგი მაგ. თქვენი სახელი, და გამოიტანეთ სახელის პირველი და ბოლო სიმბოლო
+
+name = "mari"
+print(name[0] + name[3])
+
+#შექმენით სია და სტრინგი ხოლო შემდეგ დაბეჭდეთ len ფუნქციის გამოყენებით მათი ზომა
+
+cars = ["BMW", "Mercedes", "Corvette", "ferrari"]
+print(len(cars))
+
+#შექმენით რიცხვების სია სადაც 10 რიცხვს შეიტანთ, გამოიტანეთ პირელ ინდექსა და ბოლო ინდექსზე მყოფი ელემენტების ჯამი
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(numbers[0] + numbers[9])
